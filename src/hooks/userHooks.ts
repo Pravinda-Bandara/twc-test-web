d452e6f507d28514ef8cc11154f7d7e6d03c91a5
Pravinda-Bandara <bandarapravinda@gmail.com>
2024-04-16 20:11:41 +0530

    create axios and React-Query utils

---
+++ src/hooks/userHooks.ts
@@ -0,0 +1,23 @@
+import {useMutation} from "@tanstack/react-query";
+import apiClient from "../apiClient.ts";
+
+type userID={
+    userId:string
+}
+
+export const useLoginMutation = () =>
+    useMutation({
+        mutationFn: async ({
+                               userName,
+                               userPassword,
+                           }: {
+            userName: string
+            userPassword: string
+        }) =>
+            (
+                await apiClient.post<userID>(`api/v1/users/login`, {
+                    userName,
+                    userPassword,
+                })
+            ).data,
+    })
